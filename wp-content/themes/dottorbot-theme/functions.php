c9d96a60a1eb486c9fe106b12eb6ef0b317456bb
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-12 15:33:51 +0200

    Add localization support and user preferences

--- wp-content/themes/dottorbot-theme/functions.php
+++ wp-content/themes/dottorbot-theme/functions.php
@@ -1,6 +1,11 @@
 <?php
 require_once get_template_directory() . '/api.php';
 
+function dottorbot_theme_setup() {
+    load_theme_textdomain('dottorbot', get_template_directory() . '/languages');
+}
+add_action('after_setup_theme', 'dottorbot_theme_setup');
+
 function dottorbot_enqueue_assets() {
     $theme_dir = get_template_directory_uri();
     $style_path = get_template_directory() . '/dist/style.css';
